c7570bf892e6a3cf369eb220076513b4d13f6555
Hasan Ali <hasan.ali@lacritz.com>
2021-10-16 00:36:51 +0530

    Added color box

--- src/screens/Palette.jsx
+++ src/screens/Palette.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import ColorBox from '../components/ColorBox';
+import './Palette.css';
 
 function Palette({ colors }) {
-  console.log(colors);
+  const colorBoxes = colors.map(({ color, name }) => (
+    <ColorBox background={color} key={`Item-${name}`} name={name} />
+  ));
   return (
     <div className='Palette'>
       {/* Navbar */}
-      <div className='Palette-colors'></div>
+      <div className='Palette-colors'>{colorBoxes}</div>
       {/* Footer */}
     </div>
   );
