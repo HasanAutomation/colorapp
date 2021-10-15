d715bc9dab589b307cf431209e179969fa28921a
Hasan Ali <hasan.ali@lacritz.com>
2021-10-16 00:23:57 +0530

    Added Palette component

---
+++ src/screens/Palette.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Palette({ colors }) {
+  console.log(colors);
+  return (
+    <div className='Palette'>
+      {/* Navbar */}
+      <div className='Palette-colors'></div>
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default Palette;
